1f1f3ac14245c2be674f01b7c03addbe17681711
jtriley <jtriley15@gmail.com>
2025-02-28 20:30:25 -0700

    shuffle a lil bit

--- src/LotusRouter.sol
+++ src/LotusRouter.sol
@@ -2,8 +2,17 @@
 pragma solidity 0.8.28;
 
 import { Action } from "src/types/Action.sol";
+import { BytesCalldata } from "src/types/BytesCalldata.sol";
+
 import { Error } from "src/types/Error.sol";
 import { Ptr, findPtr } from "src/types/PayloadPointer.sol";
+import { ERC20 } from "src/types/protocols/ERC20.sol";
+import { ERC6909 } from "src/types/protocols/ERC6909.sol";
+import { ERC721 } from "src/types/protocols/ERC721.sol";
+import { UniV2Pair } from "src/types/protocols/UniV2Pair.sol";
+import { UniV3Pool } from "src/types/protocols/UniV3Pool.sol";
+import { WETH } from "src/types/protocols/WETH.sol";
+import { BBCDecoder } from "src/util/BBCDecoder.sol";
 
 // +---------------------------------------------------------------------------+
 // | ## The Lotus Router Manifesto                                             |
@@ -65,7 +74,129 @@ contract LotusRouter {
         while (success) {
             (ptr, action) = ptr.nextAction();
 
-            (ptr, success) = action.execute(ptr);
+            if (action == Action.Halt) {
+                assembly {
+                    stop()
+                }
+            } else if (action == Action.SwapUniV2) {
+                bool canFail;
+                UniV2Pair pair;
+                uint256 amount0Out;
+                uint256 amount1Out;
+                address to;
+                BytesCalldata data;
+
+                (ptr, canFail, pair, amount0Out, amount1Out, to, data) =
+                    BBCDecoder.decodeSwapUniV2(ptr);
+
+                success = pair.swap(amount0Out, amount1Out, to, data) || canFail;
+            } else if (action == Action.SwapUniV3) {
+                bool canFail;
+                UniV3Pool pool;
+                address recipient;
+                bool zeroForOne;
+                int256 amountSpecified;
+                uint160 sqrtPriceLimitX96;
+                BytesCalldata data;
+
+                (
+                    ptr,
+                    canFail,
+                    pool,
+                    recipient,
+                    zeroForOne,
+                    amountSpecified,
+                    sqrtPriceLimitX96,
+                    data
+                ) = BBCDecoder.decodeSwapUniV3(ptr);
+
+                success = pool.swap(recipient, zeroForOne, amountSpecified, sqrtPriceLimitX96, data)
+                    || canFail;
+            } else if (action == Action.FlashUniV3) {
+                bool canFail;
+                UniV3Pool pool;
+                address recipient;
+                uint256 amount0;
+                uint256 amount1;
+                BytesCalldata data;
+
+                (ptr, canFail, pool, recipient, amount0, amount1, data) =
+                    BBCDecoder.decodeFlashUniV3(ptr);
+
+                success = pool.flash(recipient, amount0, amount1, data) || canFail;
+            } else if (action == Action.TransferERC20) {
+                bool canFail;
+                ERC20 token;
+                address receiver;
+                uint256 amount;
+
+                (ptr, canFail, token, receiver, amount) = BBCDecoder.decodeTransferERC20(ptr);
+
+                success = token.transfer(receiver, amount) || canFail;
+            } else if (action == Action.TransferFromERC20) {
+                bool canFail;
+                ERC20 token;
+                address sender;
+                address receiver;
+                uint256 amount;
+
+                (ptr, canFail, token, sender, receiver, amount) =
+                    BBCDecoder.decodeTransferFromERC20(ptr);
+
+                success = token.transferFrom(sender, receiver, amount) || canFail;
+            } else if (action == Action.TransferFromERC721) {
+                bool canFail;
+                ERC721 token;
+                address sender;
+                address receiver;
+                uint256 amount;
+
+                (ptr, canFail, token, sender, receiver, amount) =
+                    BBCDecoder.decodeTransferFromERC721(ptr);
+
+                success = token.transferFrom(sender, receiver, amount) || canFail;
+            } else if (action == Action.TransferERC6909) {
+                bool canFail;
+                ERC6909 multitoken;
+                address receiver;
+                uint256 tokenId;
+                uint256 amount;
+
+                (ptr, canFail, multitoken, receiver, tokenId, amount) =
+                    BBCDecoder.decodeTransferERC6909(ptr);
+
+                success = multitoken.transfer(receiver, tokenId, amount) || canFail;
+            } else if (action == Action.TransferFromERC6909) {
+                bool canFail;
+                ERC6909 multitoken;
+                address sender;
+                address receiver;
+                uint256 tokenId;
+                uint256 amount;
+
+                (ptr, canFail, multitoken, sender, receiver, tokenId, amount) =
+                    BBCDecoder.decodeTransferFromERC6909(ptr);
+
+                success = multitoken.transferFrom(sender, receiver, tokenId, amount) || canFail;
+            } else if (action == Action.DepositWETH) {
+                bool canFail;
+                WETH weth;
+                uint256 value;
+
+                (ptr, canFail, weth, value) = BBCDecoder.decodeDepositWETH(ptr);
+
+                success = weth.deposit(value) || canFail;
+            } else if (action == Action.WithdrawWETH) {
+                bool canFail;
+                WETH weth;
+                uint256 value;
+
+                (ptr, canFail, weth, value) = BBCDecoder.decodeWithdrawWETH(ptr);
+
+                success = weth.withdraw(value) || canFail;
+            } else {
+                success = false;
+            }
         }
 
         revert Error.CallFailure();
